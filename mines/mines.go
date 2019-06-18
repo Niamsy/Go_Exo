9818795ea90a0417733a1446b7c6a277fed7ac76
My-Lan Aragon <mylan.aragon@gmail.com>
2019-06-18 18:32:05 +0900

    [CHANGE] Minor print how much they produced + code refactor for mines

--- mines/mines.go
+++ mines/mines.go
@@ -20,9 +20,10 @@ type MineRequest struct {
 }
 
 type Minor struct {
-	Id int
-	ProductType MineType
-	Stack int
+	id int
+	productType MineType
+	totalProduced int
+	stack int
 }
 
 const siloSize = 30
@@ -40,17 +41,17 @@ func DefineMinors(nbAluminumMinors int, nbIronMinors int, nbTitaniumMinors int) 
 	var i = 0
 
 	for ; i < nbAluminumMinors; i++ {
-		minors[i] = Minor{Id : i + 1, ProductType : Aluminium, Stack : 0}
+		minors[i] = Minor{id: i + 1, productType: Aluminium, stack: 0, totalProduced: 0}
 	}
 
 	var maxMinorId = i + nbIronMinors
 	for ; i < maxMinorId; i++ {
-		minors[i] = Minor{Id : i + 1, ProductType : Iron, Stack : 0}
+		minors[i] = Minor{id: i + 1, productType: Iron, stack: 0, totalProduced: 0}
 	}
 
 	maxMinorId = i + nbTitaniumMinors
 	for ; i < maxMinorId; i++ {
-		minors[i] = Minor{Id : i + 1, ProductType : Titanium, Stack : 0}
+		minors[i] = Minor{id: i + 1, productType: Titanium, stack: 0, totalProduced: 0}
 	}
 
 	return minors
@@ -58,13 +59,16 @@ func DefineMinors(nbAluminumMinors int, nbIronMinors int, nbTitaniumMinors int) 
 
 // Emulates the work of a Minor
 func Produce(minor Minor, wg *sync.WaitGroup) {
-	minor.Stack += 1 // Create ore each turn
-	MineRequests <- MineRequest{Deposit: true, Ores: map[MineType]int{minor.ProductType : minor.Stack}, OresToTake:nil} //Deliver to mine
-	resp := <- MineResponses
-	minor.Stack -= resp[minor.ProductType]
+	minor.stack += 1 // Create ore each turn
+	minor.totalProduced += 1
 
-	Requests <- minor
-	respMinor := <- responses
+	MineRequests <-MineRequest{Deposit: true, Ores: map[MineType]int{minor.productType : minor.stack}, OresToTake:nil} //Deliver to mine
+	resp := <-MineResponses
+
+	minor.stack -= resp[minor.productType]
+
+	Requests <-minor
+	respMinor := <-responses
 	if respMinor == true {
 		wg.Done()
 	}
@@ -73,11 +77,11 @@ func Produce(minor Minor, wg *sync.WaitGroup) {
 func CoordinateMinors(minors []Minor) {
 	for {
 		req := <-Requests
-		if req.Id == 0 {
+		if req.id == 0 {
 			continue
 		}
-		minors[req.Id - 1].ProductType = req.ProductType
-		minors[req.Id - 1].Stack = req.Stack
+		minors[req.id - 1].stack = req.stack
+		minors[req.id - 1].totalProduced = req.totalProduced
 		responses <- true
 	}
 }
@@ -110,14 +114,14 @@ func CoordinateMines(mines map[MineType] int) {
 		req := <-MineRequests
 
 		if req.Deposit {
-			delivered := make(map[MineType]int)
+			var delivered = make(map[MineType]int)
 			delivered[Aluminium] = checkDeposit(mines, Aluminium, req.Ores[Aluminium])
 			delivered[Iron] = checkDeposit(mines, Iron, req.Ores[Iron])
 			delivered[Titanium] = checkDeposit(mines, Titanium, req.Ores[Titanium])
 
-			MineResponses <- delivered
+			MineResponses <-delivered
 		} else {
-			toGive := make(map[MineType]int)
+			var toGive = make(map[MineType]int)
 			for i := 0; i < len(req.OresToTake); i++ {
 				if checkToGive(mines, Aluminium) {
 					toGive[Aluminium] += 1
@@ -130,7 +134,7 @@ func CoordinateMines(mines map[MineType] int) {
 					continue
 				}
 			}
-			MineResponses <- toGive
+			MineResponses <-toGive
 		}
 	}
 }
@@ -146,10 +150,10 @@ func Mines() map[MineType]int {
 
 func DescribeMinor(minor Minor) {
 	var productType = "Titanium"
-	if minor.ProductType == Aluminium {
+	if minor.productType == Aluminium {
 		productType = "Aluminium"
-	} else if minor.ProductType == Iron {
+	} else if minor.productType == Iron {
 		productType = "Iron"
 	}
-	fmt.Printf("Minor %d who produced %s has in stack %d\n", minor.Id, productType, minor.Stack)
+	fmt.Printf("Minor %d who produced %d %s has in stack %d\n", minor.id, minor.totalProduced, productType, minor.stack)
 }
